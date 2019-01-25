b2961ee6dfa8575e9fb2326e16ca478bfb489566
mcbeltran <maria_beltran@javeriana.edu.co>
2019-01-25 17:09:30 -0500

    MatrizToFile added

--- src/Challenge/CompressTest.java
+++ src/Challenge/CompressTest.java
@@ -2,14 +2,57 @@ package Challenge;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 
 public class CompressTest {
 
 
+    public static void matrizToFile(int m[][], List<String> p) throws IOException {
+
+
+        // attach a file to FileWriter
+        FileWriter fw=new FileWriter("src/Challenge/pruebaMatriz.txt");
+
+
+        // into FileWriter
+
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++) {
+                fw.write(Integer.toString(m[i][j]));
+            }
+            fw.write(System.getProperty("line.separator"));
+        }
+
+        System.out.println("Writing successful");
+        //close the file
+        fw.close();
+
+
+    }
+
+
     public static void main(String[] args) throws IOException {
+
+        int [][] matriz = new int[6][6];
+        List<String > paises = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                matriz[i][j] = 0;
+            }
+        }
+        paises.add("Colombia");
+        paises.add("USA");
+        paises.add("Argentina");
+        paises.add("Inglaterra");
+        paises.add("Canada");
+
+
+        matrizToFile(matriz, paises);
 
 
         //-------------------------COMPRIMIR-------------------------------
